72ae067da406172cd29f66f76a04fe5175389f68
Gordon Hew <gordonhew@gmail.com>
2014-04-28 22:46:18 -0400

    Addresses Session 2 Complete

--- Addresses/Addresses/MainWindow.xaml.cs
+++ Addresses/Addresses/MainWindow.xaml.cs
@@ -22,21 +22,28 @@ namespace Addresses
     {
         Entries AddressList = new Entries();
         private int current = 0;
+        private Entry[] SelectedEntries;
         
         public MainWindow()
         {
             InitializeComponent();
+            SelectedEntries = AddressList.ToArray();
             Display();
         }
 
         void Display()
         {
-            Entry e = AddressList[current];
+            Entry e = SelectedEntries[current];
             tbName.Text = e.Name;
             tbAddress.Text = e.Address;
             tbCSZ.Text = e.CSZ;
             tbPhone.Text = e.Phone;
             tbEmail.Text = e.Email;
+
+            lblRecord.Content = "Record " + (current + 1) + " of " + SelectedEntries.Length;
+
+            btnStart.IsEnabled = btnPrevious.IsEnabled = (current != 0);
+            btnEnd.IsEnabled = btnNext.IsEnabled = (current != SelectedEntries.Length - 1);
         }
 
         private void btnStart_Click(object sender, RoutedEventArgs e)
@@ -54,15 +61,47 @@ namespace Addresses
 
         private void btnNext_Click(object sender, RoutedEventArgs e)
         {
-            if (current < AddressList.Count - 1)
+            if (current < SelectedEntries.Length - 1)
                 current++;
             Display();
         }
 
         private void btnEnd_Click(object sender, RoutedEventArgs e)
         {
-            current = AddressList.Count - 1; 
+            current = SelectedEntries.Length - 1; 
             Display();
+        }
+
+        private void FindBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (FindBtn.Content.Equals("Reset") || String.Empty.Equals(tbFind.Text))
+            {
+                SelectedEntries = AddressList.ToArray();
+                FindBtn.Content = "Find";
+                current = 0;
+            }
+            else
+            {
+                Entry[] newSelection = (from selected in AddressList where selected.Name.ToUpper().Contains(tbFind.Text.ToUpper()) select selected).ToArray();
+
+                if (!newSelection.Any())
+                {
+                    MessageBox.Show("Cannot find records that match " + tbFind.Text);
+                    return;
+                }
+                else
+                {
+                    SelectedEntries = newSelection;
+                    current = 0;
+                }
+            }
+            
+            Display();
+        }
+
+        private void tbFind_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FindBtn.Content = SelectedEntries.Length != AddressList.Count && String.Empty.Equals(tbFind.Text) ? "Reset" : "Find";
         }
     }
 }
